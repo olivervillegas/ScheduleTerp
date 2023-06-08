20695c32f688afd187b06028cec45778e3b3adbb
GidntSquia <jaxondlee@gmail.com>
2023-06-08 10:10:32 -0400

    Add comment

--- ScheduleTerp.py
+++ ScheduleTerp.py
@@ -118,6 +118,7 @@ class Section:
     Returns:
         float: The average GPA of the Professor.
     """
+    # TODO account for W's here as well
     past_sections = [d for d in grades_dict if d.get('professor') == prof]
     GPA_weights   = [4.0,4.0,3.7, 3.3,3.0,2.7, 2.3,2.0,1.7, 1.3,1.0,0.7, 0.0]
     grades        = [0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,0.0, 0.0]
