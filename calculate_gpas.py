87a05363128be127dfe5d72585ffbb851713f131
dotcomstar <gamestar109@live.com>
2023-06-14 17:16:10 -0700

    Create files

---
+++ calculate_gpas.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Generate a dictionary of all the professors for the upcoming semester
+so we can ship the data live. This will save computation time at runtime.
+"""
+
+import numpy as np
+import re
+import requests
+import random
+__author__ = "Jet Lee"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Oliver Villegas, Jaxon Lee"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Jet Lee"
+__email__ = "jetrlee@gmail.com"
+__status__ = "Development"
+
+
+headers = {
+    'Accept': 'application/json'
+}
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
